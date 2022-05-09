67e46e0ced1e03876349ff99c5d265bf9796b77c
Cromulent74 <ilya.fomko03@mail.ru>
2022-05-09 12:35:50 +0500

    integral of Simpson

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using tf = double (*)(double x);
 
 double integralTrapezoid(double a, double b, double e, tf f) {
-    double sum = 0, prevS = 0, h = b - a, curS = h * (f(a) + f(b)) / 2;
+    double sum = (f(a) + f(b)) / 2, prevS = 0, h = b - a, curS = h * sum;
 
     do {
         prevS = curS;
@@ -14,41 +14,44 @@ double integralTrapezoid(double a, double b, double e, tf f) {
 
         h /= 2;
 
-        curS = (((f(a) + f(b)) / 2 + sum) * h);
-    } while (fabs(curS - prevS) >= e);
-
-    return curS;
-}
-
-double integralTrapezoid2(double a, double b, double e, tf f) {
-    double prevS = 0, curS = (b - a) * (f(a) + f(b)) / 2;
-    int n = 1;
-
-    do {
-        std::cout << curS << std::endl;
-        n *= 2;
-        prevS = curS;
-
-        curS = (f(a) + f(b)) / 2;
-
-        for (int i = 1; i < n - 1; i++)
-            curS += (f(a + i * (b-a) / n));
-
-        curS *= ((b-a) / n);
+        curS = (sum * h);
     } while (fabs(curS - prevS) >= e);
 
     return curS;
 }
 
 double integralSimpson(double a, double b, double e, tf f) {
+    int n = 0;
+    double h = b - a, prevSum = 0, curSum = h / 3 * (f(a) + f(b));
 
+    do {
+        prevSum = curSum;
+        curSum = f(a) + f(b);
+        h /= 2;
+
+        int index = 0;
+
+        for (double x = a + h; x < b; x += h) {
+            index++;
+
+            if (index % 2 == 1)
+                curSum += 4 * f(x);
+            else
+                curSum += 2 * f(x);
+        }
+
+        curSum *= h;
+        curSum /= 3;
+    } while (fabs(prevSum - curSum) >= e);
+
+    return curSum;
 }
 
 double exF1(double x) {
-    return 1 / (x * x);
+    return x*x;
 }
 
 int main() {
-    std::cout << integralTrapezoid(1, 5, 0.001, exF1) << std::endl;
+    std::cout << integralSimpson(0, 2, 0.001, exF1) << std::endl;
     return 0;
 }
